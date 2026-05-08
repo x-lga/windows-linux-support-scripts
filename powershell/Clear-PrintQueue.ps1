005fe8e0bc02498d14ddb1e8a8dbd15d1e086b0a
x-lga <lga.it@proton.me>
2026-05-08 21:54:06 +0300

    Add Clear print queue - what ticket this solves

---
+++ powershell/Clear-PrintQueue.ps1
@@ -0,0 +1,7 @@
+<# **What ticket this solves:** "I can't print - there's a job stuck in the queue" is
+one of the most frequent L1 help desk calls. A stuck print job blocks all subsequent
+jobs in the queue. Clearing it manually requires opening Services, stopping the
+Print Spooler, navigating to the spool folder, deleting files, and restarting
+the service - a 3-minute process with multiple steps that technicians often get
+wrong (forgetting to stop the service before deleting files, or not restarting it). #>
+
