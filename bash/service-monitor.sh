a5d0d5b0fce93aa0bb41b2ee5a5b57376a16fb82
x-lga <lga.it@proton.me>
2026-05-10 20:39:54 +0300

    Add cert alignment - service monitor

--- bash/service-monitor.sh
+++ bash/service-monitor.sh
@@ -2,3 +2,6 @@
 # whether critical services are active, restarting them if they have stopped, and
 # logging the result. This is a common NOC and monitoring task that is also useful
 # as a cron-based watchdog script.
+
+# **Cert alignment:** CompTIA A+, CompTIA Network+
+
